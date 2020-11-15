e5cbc04fa222f7d64bb043b1db1b33f59bbe4979
Vo Bao <vobao031298@gmail.com>
2020-11-15 13:17:13 +0700

    update

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -2,8 +2,35 @@ require('dotenv').config();
 
 const MY_VERIFY_FB_TOKEN = process.env.MY_VERIFY_FB_TOKEN;
 
+function handleMessage(sender_psid, received_message) {
+
+  let response;
+
+  // Check if the message contains text
+  if (received_message.text) {    
+
+    // Create the payload for a basic text message
+    response = {
+      "text": `You sent the message: "${received_message.text}". Now send me an image!`
+    }
+  }  
+  
+  // Sends the response message
+  callSendAPI(sender_psid, response);    
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+  
+}
+
 let postWebhook = (req, res)=>{
-     // Parse the request body from the POST
+  // Parse the request body from the POST
   let body = req.body;
 
   // Check the webhook event is from a Page subscription
