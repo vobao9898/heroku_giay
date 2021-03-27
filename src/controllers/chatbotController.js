1b2f274e414dbbb6c5a4c358caf925b98d281e6c
Vo Bao <vobao031298@gmail.com>
2021-03-27 13:43:25 +0700

    update

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -10,18 +10,21 @@ let postWebhook = (req, res) => {
     // Check the webhook event is from a Page subscription
     if (body.object === 'page') {
         // Iterate over each entry - there may be multiple if batched
-        body.entry.forEach(function(entry) {
-            // Gets the body of the webhook event
-            let webhook_event = entry.messaging[0];
-            console.log(webhook_event);
+        // Gets the body of the webhook event
+        let webhook_event = entry.messaging[0];
+        console.log(webhook_event);
 
-            // Get the sender PSID
-            let sender_psid = webhook_event.sender.id;
-            console.log('Sender PSID: ' + sender_psid);
-        });
+        // Get the sender PSID
+        let sender_psid = webhook_event.sender.id;
+        console.log('Sender PSID: ' + sender_psid);
 
-        // Return a '200 OK' response to all events
-        res.status(200).send('EVENT_RECEIVED');
+        // Check if the event is a message or postback and
+        // pass the event to the appropriate handler function
+        if (webhook_event.message) {
+            handleMessage(sender_psid, webhook_event.message);
+        } else if (webhook_event.postback) {
+            handlePostback(sender_psid, webhook_event.postback);
+        }
     } else {
         // Return a '404 Not Found' if event is not from a page subscription
         res.sendStatus(404);
