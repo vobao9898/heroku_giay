2f7e4114d1abc05a126653d187b4337fd1d27771
Vo Bao <vobao031298@gmail.com>
2021-03-27 13:22:10 +0700

    update

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import homepageController from '../controllers/homepageController';
-// import chatbotController from '../controllers/chatbotController';
+import chatbotController from '../controllers/chatbotController';
 let router = express.Router();
 
 let initWebRouter = (app) => {
     router.get('/', homepageController.getHomepage);
 
-    // router.get('/webhook', chatbotController.getWebhook);
-    // router.post('/webhook', chatbotController.postWebhook);
+    router.get('/webhook', chatbotController.getWebhook);
+    router.post('/webhook', chatbotController.postWebhook);
     return app.use('/', router);
 };
 
