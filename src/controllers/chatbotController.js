a5007d0111d03d0fae6ab97d4831f244bf34174d
Vo Bao <vobao031298@gmail.com>
2021-03-27 13:47:25 +0700

    update

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -56,14 +56,15 @@ let getWebhook = (req, res) => {
 function handleMessage(sender_psid, received_message) {
     let response;
 
-    // Check if the message contains text
+    // Checks if the message contains text
     if (received_message.text) {
-        // Create the payload for a basic text message
+        // Create the payload for a basic text message, which
+        // will be added to the body of our request to the Send API
         response = {
-            text: `You sent the message: "${received_message.text}". Now send me an image!`,
+            text: `You sent the message: "${received_message.text}". Now send me an attachment!`,
         };
     } else if (received_message.attachments) {
-        // Gets the URL of the message attachment
+        // Get the URL of the message attachment
         let attachment_url = received_message.attachments[0].payload.url;
         response = {
             attachment: {
@@ -91,12 +92,26 @@ function handleMessage(sender_psid, received_message) {
         };
     }
 
-    // Sends the response message
+    // Send the response message
     callSendAPI(sender_psid, response);
 }
 
 // Handles messaging_postbacks events
-function handlePostback(sender_psid, received_postback) {}
+function handlePostback(sender_psid, received_postback) {
+    let response;
+
+    // Get the payload for the postback
+    let payload = received_postback.payload;
+
+    // Set the response based on the postback payload
+    if (payload === 'yes') {
+        response = { text: 'Thanks!' };
+    } else if (payload === 'no') {
+        response = { text: 'Oops, try sending another image.' };
+    }
+    // Send the message to acknowledge the postback
+    callSendAPI(sender_psid, response);
+}
 
 // Sends response messages via the Send API
 function callSendAPI(sender_psid, response) {
