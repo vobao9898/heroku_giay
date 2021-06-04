4240ac810cc953fdab0f09683d43284433876328
Vo Bao <vobao031298@gmail.com>
2021-06-04 17:01:26 +0700

    update

--- src/server.js
+++ src/server.js
@@ -24,13 +24,16 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 global.__basedir = __dirname;
-const corsOptions = {
-    origin: 'https://utc9898.000webhostapp.com',
-    credentials: true, //access-control-allow-credentials:true
-    optionSuccessStatus: 200,
-};
 
-app.use(cors(corsOptions));
+app.use(
+    cors({
+        allowedHeaders: ['sessionId', 'Content-Type'],
+        exposedHeaders: ['sessionId'],
+        origin: '*',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        preflightContinue: false,
+    })
+);
 //config view engine
 configViewEngine(app);
 
