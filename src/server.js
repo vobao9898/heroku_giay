322bacbd358734ad53b058dfb66f4528d1b70d2f
Vo Bao <vobao031298@gmail.com>
2021-06-04 17:08:26 +0700

    update

--- src/server.js
+++ src/server.js
@@ -24,16 +24,21 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 global.__basedir = __dirname;
+const corsOptions = {
+    origin: '*',
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+};
 
-app.use(
-    cors({
-        allowedHeaders: ['sessionId', 'Content-Type'],
-        exposedHeaders: ['sessionId'],
-        origin: '*',
-        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-        preflightContinue: false,
-    })
-);
+// app.use(
+// 	cors({
+// 		allowedHeaders: ['sessionId', 'Content-Type'],
+// 		exposedHeaders: ['sessionId'],
+// 		origin: '*',
+// 		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+// 		preflightContinue: false,
+// 	})
+// );
 //config view engine
 configViewEngine(app);
 
