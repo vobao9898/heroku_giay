ccec6a0fbaf4466594968886742562213e7bf56a
Vo Bao <vobao031298@gmail.com>
2021-06-13 12:22:19 +0700

    update

--- src/api/san_pham/sanpham.service.js
+++ src/api/san_pham/sanpham.service.js
@@ -106,7 +106,7 @@ module.exports = {
 
     newProductsAll: (callBack) => {
         pool.query(
-            `select g.id, g.ten_giay, g.gia_ban, g.id_loai_giay, g.mo_ta, g.gia_ban_khuyen_mai, m.id, m.id_mau_sac, ms.ten_mau_sac, m.hinh_anh, s.id_size, si.ten_size, s.so_luong from giay as g, chi_tiet_mau_sac as m, chi_tiet_mau_sac_size as s, mau_sac as ms, size as si WHERE g.id = m.id_giay  and m.id_mau_sac = ms.id and  m.id = s.id_ct_mau_sac and si.id = s.id_size and g.trang_thai != 0 and g.id IN (SELECT sub.id from (select id from giay ORDER BY ? ? LIMIT ? OFFSET ?)AS sub))`, [data.sortBy, data.groupBy, data.limit, data.offset],
+            `select g.id, g.ten_giay, g.gia_ban, g.id_loai_giay, g.mo_ta, g.gia_ban_khuyen_mai, m.id, m.id_mau_sac, ms.ten_mau_sac, m.hinh_anh, s.id_size, si.ten_size, s.so_luong from giay as g, chi_tiet_mau_sac as m, chi_tiet_mau_sac_size as s, mau_sac as ms, size as si WHERE g.id = m.id_giay  and m.id_mau_sac = ms.id and  m.id = s.id_ct_mau_sac and si.id = s.id_size and g.trang_thai != 0 and g.id IN (SELECT sub.id from (select id from giay ORDER BY ? ? LIMIT ? OFFSET ?)AS sub`, [data.sortBy, data.groupBy, data.limit, data.offset],
             (error, results, fields) => {
                 if (error) {
                     callBack(error);
