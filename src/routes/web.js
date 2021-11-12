138d94a6d8369c4dd0d49a4bb19aa67388cd0bbb
Vo Bao <vobao031298@gmail.com>
2021-11-12 20:28:35 +0700

    update

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,15 +1,10 @@
 import express from 'express';
-import homepageController from '../controllers/homepageController';
-import chatbotController from '../controllers/chatbotController';
 
 import sql from './../sql/sql';
 
 let router = express.Router();
 
 let initWebRouter = (app) => {
-    //   router.get('/', homepageController.getHomepage);
-    router.get('/webhook', chatbotController.getWebhook);
-    router.post('/webhook', chatbotController.postWebhook);
     router.get('/api/nhan-vien', sql.danhsachLogin);
 
     router.get('/logout', (req, res) => {
