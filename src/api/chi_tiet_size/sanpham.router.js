8b6e840d85b3516dd030a40eb061f794fea5054e
Vo Bao <vobao031298@gmail.com>
2021-11-20 17:18:17 +0700

    update

--- src/api/chi_tiet_size/sanpham.router.js
+++ src/api/chi_tiet_size/sanpham.router.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { checkToken } = require('../../auth/token_validation');
 import * as ctmauSac from './sanpham.controller';
 router.get('/', ctmauSac.getAll);
-router.get('/getSize', ctmauSac.getSize);
+router.post('/getSize', ctmauSac.getSize);
 // router.get('/page', ctmauSac.page);
 router.post('/', ctmauSac.create);
 // router.get('/:id', ctmauSac.getById);
--- src/api/chi_tiet_size/sanpham.service.js
+++ src/api/chi_tiet_size/sanpham.service.js
@@ -48,7 +48,7 @@ module.exports = {
                 if (error) {
                     callBack(error);
                 }
-                return callBack(null, results[0]);
+                return callBack(null, results);
             }
         );
     },
