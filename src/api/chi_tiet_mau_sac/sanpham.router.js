3c46185dc1e920cef7da375dc0e304113020908f
vobao9898 <vobao9898@gmail.com>
2021-12-18 19:09:34 +0700

    update

--- src/api/chi_tiet_mau_sac/sanpham.router.js
+++ src/api/chi_tiet_mau_sac/sanpham.router.js
@@ -1,11 +1,11 @@
-const router = require('express').Router();
-const { checkToken } = require('../../auth/token_validation');
-import * as ctmauSac from './sanpham.controller';
-router.get('/', ctmauSac.getAll);
-router.get('/page', ctmauSac.page);
-router.post('/', ctmauSac.create);
+const router = require("express").Router();
+const { checkToken } = require("../../auth/token_validation");
+import * as ctmauSac from "./sanpham.controller";
+router.get("/", ctmauSac.getAll);
+router.get("/page", ctmauSac.page);
+router.post("/", ctmauSac.create);
 
-router.patch('/', ctmauSac.update);
-router.post('/delete', ctmauSac.delete);
+router.patch("/", ctmauSac.update);
+router.post("/delete", ctmauSac.delete);
 
 module.exports = router;
